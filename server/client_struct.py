3f1bad30d3af8c04f7ff4d5691fabb923f4f0f88
yuchi627 <g870627@gmailcom>
2019-05-09 01:17:48 +0800

    send img

--- server/client_struct.py
+++ server/client_struct.py
@@ -2,19 +2,23 @@ import numpy as np
 import cv2
 class client:
     def __init__(self):
-        self.package = -1
+        self.total_package_size = -1
         self.img = b''
-    def p(self):
-        print("client!!!!!!")
+    def package_size(self):
+        if(self.total_package_size >= 1024):
+            return 1024
+        else :
+            return self.total_package_size
     def package_value(self):
-        return self.package
+        return self.total_package_size
     def package_set(self,package_num):
-        self.package = package_num
-    def package_decrease(self):
-        self.package -= 1
+        self.total_package_size = package_num
+    def package_decrease(self, decrease_num):
+        self.total_package_size -= decrease_num
     def img_combine(self,recv_str):
         self.img += recv_str
     def img_read(self):
         data = np.fromstring(self.img, dtype = 'uint8')
         data = cv2.imdecode(data,1)
+        self.img = b''
         return np.reshape(data,(480,640,3))
--- server/multiconn-server.py
+++ server/multiconn-server.py
@@ -39,47 +39,35 @@ def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
-        if(client_dict[str(data.addr)].package_value() == -1):
+        if(client_dict[str(data.addr)].package_value() < 0):
             recv_data = sock.recv(16)
-            package_num = int(recv_data.decode())
-            client_dict[str(data.addr)].package_set(package_num)
+            print("ori: ",recv_data)
+            print("deocde",recv_data.decode())
+            package_num = recv_data.decode()
+            if(package_num == "SOS"):
+                print("Save him!!!!!!!!!!!!!!!!!!!!!!!!!")
+            else:
+                client_dict[str(data.addr)].package_set(int(package_num))
         else:
-            recv_data = sock.recv(1024)
+            recv_data = sock.recv(client_dict[str(data.addr)].package_size())
+            print("recv: ",len(recv_data))
+            print(client_dict[str(data.addr)].package_size())
             #recv package and concatenate recv msg to img
             client_dict[str(data.addr)].img_combine(recv_data)
-            client_dict[str(data.addr)].package_decrease()
-            if(client_dict[str(data.addr)].package_value() == 0):
+            client_dict[str(data.addr)].package_decrease(len(recv_data))
+            if(client_dict[str(data.addr)].package_value() <= 0):
                 img = client_dict[str(data.addr)].img_read()
                 client_dict[str(data.addr)].package_set(-1)
                 cv2.imwrite("img.jpg",img)
+                print("img recv ")
                 cv2.imshow(str(data.addr),img)
-        '''
-        length = sock.recv(16)
-        print(length)
-        print("decode",length.decode())
-        l = int(length.decode())
-        stringData = sock.recv(1024)
-        print(len(stringData),"stringData: ")
-        for i in range(1,l):
-            stringData += sock.recv(1024)
-            print("stringData",len(stringData))
-        #stringData = recvall(sock,int(length.decode()))
-        data = np.fromstring(stringData, dtype = 'uint8')
-        print("data: ",data)
-        #data = np.reshape(data,(640,480))
-        cv2.imwrite("data.jpg",data)
-        decimg = cv2.imdecode(data,1)
-        cv2.imwrite("recv.jpg",decimg)
-        print("img:  ",decimg)
-        cv2.imshow('client',decimg)
-        '''
-        #recv_data = sock.recv(1024)  # Should be ready to read
-        #if decimg.any:
-        #    data.outb = decimg
+                cv2.waitKey(1)
         if recv_data:
-            data.outb += recv_data
+            pass
+            #data.outb += recv_data
         else:
             print('closing connection to', data.addr)
+            cv2.destroyWindow(str(data.addr))
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
@@ -93,7 +81,6 @@ def service_connection(key, mask):
 
 try:
     client_dict = {"client":client_struct.client()}
-    client_dict["client"].p()
     sel = selectors.DefaultSelector()
 
     lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -102,7 +89,6 @@ try:
     print('listening on', (host, port))
     lsock.setblocking(False)
     sel.register(lsock, selectors.EVENT_READ, data=None)
-    cv2.namedWindow('client')
     while True:
         events = sel.select(timeout=None)
         for key, mask in events:
@@ -110,7 +96,8 @@ try:
                 accept_wrapper(key.fileobj)
             else:
                 service_connection(key, mask)
-        cv2.waitKey(1)
+        #cv2.waitKey(1)
 finally:
     lsock.close()
+    cv2.destroyAllWindows()
     print("close socket")
