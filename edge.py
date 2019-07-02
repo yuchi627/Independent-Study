1bd3fa29183a735fc440ffa64eddcb1c9605f53d
JCP1014 <believe861014@gmail.com>
2019-07-02 15:43:44 +0800

    edge

--- edge.py
+++ edge.py
@@ -9,6 +9,7 @@ import subprocess
 from scikit import compare_ssim
 from pylepton import Lepton
 import select
+import picamera.array
 
 HOST = '192.168.68.196'
 PORT = 6667
@@ -34,14 +35,25 @@ def classfy(flir_val):
     th_100 = 7700       #9650
     flir_val = cv2.resize(flir_val,(640,480))
     flir_val = np.dstack([flir_val]*3)
-    first = np.where(flir_val < th_70,(255,0,0), 0)
-    second_1 = np.where(flir_val >= th_70, (0,255,0), 0)
-    second_2 = np.where(flir_val >= th_100, 0, (0,255,0))
-    second = np.bitwise_and(second_1,second_2)
-    third = np.where(flir_val >= th_100, (0,0,255),0)
+    tn1 = time.time()
+    '''
+    first = flir_val.copy()
+    second = flir_val.copy()
+    third = flir_val.copy()
+    np.place(first, (first < th_70 ),(255,0,0))
+    np.place(first, (first >= th_70 ),(0,0,0))
+    np.place(second, ((second >= th_70 ) & (second < th_100)),(0,255,0))
+    np.place(second, ((second < th_70 ) | (second >= th_100)),(0,0,0))
+    np.place(third, (third >= th_100 ),(0,0,255))
+    np.place(third, (third < th_100 ),(0,0,0))
     fusion = first + second + third
+    '''
+    #fusion = np.where(flir_val < th_70,(255,0,0), np.where(flir_val >= th_100, (0,0,255),(0,255,0)))
+    fusion = np.where(flir_val >= th_100, (0,0,255),0)
+    tn2 = time.time()
+    print("classfy time = ",tn2-tn1)
+    #fusion = first + second + third
     return fusion.astype(np.float32)
-
 
 def sobel(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -53,21 +65,23 @@ def sobel(img):
     sobel_combine = cv2.bitwise_or(sobelx,sobely)
     return sobel_combine
 
-def img_processing(ir_img,flir_val,flir_img):
-        global img_combine,refresh 
-        flir_img = cv2.applyColorMap(flir_img, cv2.COLORMAP_JET)
-        #cv2.imwrite('flir1.jpg',flir_img)
-        flir_img = cv2.resize(flir_img,(640,480))
-        ir_img = cv2.resize(ir_img,(640,480))
-        ir_img = sobel(ir_img)
-        ir_img = np.dstack([ir_img]*3)
-        white = ir_img.copy()
-        classfy_color = classfy(flir_val)
-        flir_tmp = cv2.resize(classfy_color,(ir_weight-120,ir_height-100),interpolation = cv2.INTER_CUBIC)
-        #flir_tmp = cv2.resize(flir_img,(ir_weight-120,ir_height-100),interpolation = cv2.INTER_CUBIC)
-        white[move_y:move_y+flir_height,move_x:move_x+flir_weight] = flir_tmp
-        img_combine = cv2.addWeighted(ir_img,0.8,white,0.2,0)
-        refresh = True
+def img_processing(ir_img,flir_val):
+    global img_combine,refresh 
+    tn1 = time.time()
+    #ir_img = cv2.resize(ir_img,(640,480))
+    ir_img = cv2.cvtColor(ir_img,cv2.COLOR_BGR2GRAY)
+    ir_img = np.dstack([ir_img]*3)
+    tmp = ir_img.copy()
+    flir_val = cv2.resize(flir_val,(ir_weight-120,ir_height-100),interpolation = cv2.INTER_CUBIC)
+    white = np.zeros((480,640),np.uint16)
+    white[move_y:move_y+flir_height,move_x:move_x+flir_weight] = flir_val
+    white = np.dstack([white]*3)
+    np.place(tmp,(white>th_100),(0,0,255))
+    img_combine = cv2.addWeighted(ir_img,0.8,tmp,0.2,0)
+    tn2 = time.time()
+    print("convert = ",tn2-tn1)
+    refresh = True
+
 
 count = 0
 cv2.namedWindow('picamera',cv2.WINDOW_AUTOSIZE)
@@ -82,9 +96,11 @@ flir_height = 380   #flir_tmp.shape[0]
 flir_weight = 520   #flir_tmp.shape[1]
 move_y = 24
 move_x = 50
-
+th_70 = 7680        #8900
+th_100 = 7700       #9650
 refresh = False
 img_combine = np.zeros((ir_height,ir_weight,3),np.uint8)
+
 try:
     camera = picamera.PiCamera()
     camera.resolution = (640,480)
@@ -92,6 +108,8 @@ try:
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),20]
     s.send(("Nadine").ljust(16).encode())
     save_msg_count = 0
+    stream = picamera.array.PiRGBArray(camera)
+
     #pic_count = 0
     while True:
     #while(pic_count<100):
@@ -99,11 +117,16 @@ try:
         t0 = time.time()
         flir_img, flir_val = capture()
         t1 = time.time()
-        camera.capture("ir1.jpg",use_video_port = True)
+
+        camera.capture(stream,format= 'bgr' ,use_video_port = True)
+        tmp1 = stream.array
+        stream.truncate(0)
+        #camera.capture("ir1.jpg",use_video_port = True)
+        #tmp1 = cv2.imread('ir1.jpg')
+
         t2 = time.time()
         print("flir_time = ",t1-t0)
         print("ir_time = ",t2-t1)
-        tmp1 = cv2.imread('ir1.jpg')
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),90]
         result, imgencode = cv2.imencode('.jpg',tmp1,encode_param)
         data = np.array(imgencode)
@@ -111,8 +134,11 @@ try:
         s.send(str(len(stringData)).ljust(16).encode())
         s.send(stringData)
         
-        thread_img = threading.Thread(target = img_processing,args=(tmp1,flir_val,flir_img))
+        
+        print("before thread")
+        thread_img = threading.Thread(target = img_processing,args=(tmp1,flir_val,))
         thread_img.start()
+        print("after thread")
         if(refresh):
             refresh = False
             cv2.imshow("combine",img_combine)
@@ -122,6 +148,7 @@ try:
         cv2.imshow('picamera',tmp1)
         cv2.waitKey(1)
         '''
+        
 finally:
         camera.close()
         s.close()
