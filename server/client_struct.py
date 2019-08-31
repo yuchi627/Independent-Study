884cdb58f45c6b749a7f31857e537235787599d8
YuchiWang <g870627@gmail.com>
2019-08-31 16:21:18 +0800

    Update client_struct.py

--- server/client_struct.py
+++ server/client_struct.py
@@ -3,74 +3,78 @@ import cv2
 import struct
 import time
 from queue import Queue
-height = 480
-weight = 640
-name_space_height = 50
-
-img_white = np.zeros((height+name_space_height,weight,3), np.uint8)
-img_white[:,:] = (255,255,255)
-
-img_white_namespace = np.zeros((name_space_height,weight,3), np.uint8)
-img_white_namespace[:,:] = (255,255,255)
-
-bound_w = 1174 
-bound_h = 705 
-right_x = 25 + bound_w
-bottom_y = 25 + bound_h
-line_W = 10
-
-middle_x = 1170
-middle_y = 700
-matrix = np.loadtxt("matrix6.txt", delimiter=',')
-M = cv2.getRotationMatrix2D((weight/2,height/2), 180, 1)
 
 class client:
-    count_data = 0
-    record_flag = False
-    img_count = 0
-    th_70 = 0   ###### threshold for 70 degree flir value
-    th_100 = 0  ###### threshold for 100 degree flir value
-    remain_package_size = 0
+    ###### image setting
+    height = 480
+    weight = 640
+    name_space_height = 50
+    bound_w = 1174 
+    bound_h = 705 
+    right_x = 25 + bound_w
+    bottom_y = 25 + bound_h
+    line_W = 10
+    middle_x = 1170
+    middle_y = 700
+    matrix = np.loadtxt("matrix6.txt", delimiter=',')
+    M = cv2.getRotationMatrix2D((weight/2,height/2), 180, 1)
+    ###### for namespace
+    name = "name"
+    first_flag = False  ###### first time recv msg
+    img_white = np.zeros((height+name_space_height,weight,3), np.uint8)
+    img_white[:,:] = (255,255,255)
+    img_white_namespace = np.zeros((name_space_height,weight,3), np.uint8)
+    img_white_namespace[:,:] = (255,255,255)
+    ###### for image recv
+    remain_package_size = 0     
     img_package_size = 0
+    img_binary = b''
+    ###### for image send
     send_package_size = 0
     sended_size = 0
-    remain_send_size = 0
-    img_binary = b''
+    send_img_flag = False
+    ###### for message recv
     msg_binary = b''
     msg_size = 16
+    remain_send_size = img_package_size
     remain_msg_size = msg_size
+    ###### for show image 
     img_ir = img_white.copy()
     img_combine = img_white.copy()
     img_show = img_white.copy()
-    name = "name"
     recv_ir_flag = False
     recv_flir_flag = False
     visible_flag = False
-    first_flag = False  ###### first time recv msg
+    th_70 = 0   ###### threshold for 70 degree flir value
+    th_100 = 0  ###### threshold for 100 degree flir value
+    ###### for sos draw namespace background
+    first_recv_sos = True
     sos_flag = False
     twinkling_flag = False
+    ###### put text on image
     closing_danger_flag = False     ###### close to the danger area
     in_danger_flag = False      ###### the red area more than one third of pic
     in_explosion_flag = False   ###### in the area which commander select
     send_save_msg_flag = False  
     send_over_time_flag = False
-    send_img_flag = False
-    disconnect_flag = True
-    set_start = False       
-    fireman_bound_top = 0   ###### bound of drawing fireman picture on map
+    draw_count = 0      ###### count the emergency message number
+    ###### for replay image 
+    disconnect_flag = True  
+    back_img_count = 0
+    ###### bound of drawing fireman picture on map  
+    fireman_bound_top = 0   
     fireman_bound_bottom = 0
     fireman_bound_left = 0
     fireman_bound_right = 0
-    explosion_bound_top = 0     ###### bound of selecting explosion area on map
+    ###### bound of selecting explosion area on map
+    explosion_bound_top = 0     
     explosion_bound_bottom = 0
     explosion_bound_left = 0
     explosion_bound_right = 0
-    draw_count = 0      ###### count the emergency message number
-    back_img_count = 0
 # ---------------------------------------------#
     color_set = (0,0,0) # 紅綠燈的燈號
     fire_num = ""
-    fire_name = ""
+    fire_name = ""          #########################################delete
     time_in = 0
     time_pass = 0
     id_num = 0 # 顯示在Map的數字
@@ -102,16 +106,18 @@ class client:
     blink_count = 0
     disconnect_time = 0
     disconnect_real_time = 0
+    set_start = False   
+    help2_flag = False
 #------------------------------------------------#
     def __init__(self, num, queue_number):
         self.number = num
         self.first_flag = True
         #self.file = open(str(num)+".txt","w")
-        self.namespace_img = img_white_namespace
-        self.left_spot_x = 5 + (middle_x-5)*(num%2)
-        self.right_spot_x = middle_x + middle_x*(num%2)
-        self.up_spot_y = 5 + (middle_y-5)*(num >= 2)
-        self.down_spot_y = middle_y + (middle_y)*(num >= 2)
+        self.namespace_img = self.img_white_namespace
+        self.left_spot_x = 5 + (self.middle_x-5)*(num%2)
+        self.right_spot_x = self.middle_x + self.middle_x*(num%2)
+        self.up_spot_y = 5 + (self.middle_y-5)*(num >= 2)
+        self.down_spot_y = self.middle_y + (self.middle_y)*(num >= 2)
         self.line_left_spot_x = self.left_spot_x
         self.line_right_spot_x = self.right_spot_x
         self.line_up_spot_y = self.up_spot_y
@@ -126,10 +132,10 @@ class client:
             self.right_thickness = self.thin
             self.down_thickness = self.thin
 
-            self.explosion_bound_top = line_W
-            self.explosion_bound_bottom = bound_h - line_W
-            self.explosion_bound_left = line_W
-            self.explosion_bound_right = bound_w - line_W
+            self.explosion_bound_top = self.line_W
+            self.explosion_bound_bottom = self.bound_h - self.line_W
+            self.explosion_bound_left = self.line_W
+            self.explosion_bound_right = self.bound_w - self.line_W
             
         elif(num == 1):
             self.line_left_spot_x = self.line_left_spot_x + self.line_offset
@@ -137,12 +143,12 @@ class client:
             self.left_thickness = self.thin
             self.down_thickness = self.thin
 
-            self.explosion_bound_top = line_W
-            self.explosion_bound_bottom = bound_h - line_W
-            self.explosion_bound_left = bound_w 
-            self.explosion_bound_right = bound_w * 2 - int(line_W * 1.5)
+            self.explosion_bound_top = self.line_W
+            self.explosion_bound_bottom = self.bound_h - self.line_W
+            self.explosion_bound_left = self.bound_w 
+            self.explosion_bound_right = self.bound_w * 2 - int(self.line_W * 1.5)
 
-            self.position_x = right_x
+            self.position_x = self.right_x
             
         elif(num == 2):
             self.line_right_spot_x = self.line_right_spot_x - self.line_offset
@@ -150,12 +156,12 @@ class client:
             self.right_thickness = self.thin
             self.up_thickness = self.thin
 
-            self.explosion_bound_top = bound_h
-            self.explosion_bound_bottom = bound_h * 2 - int(line_W * 1.5)
-            self.explosion_bound_left = line_W
-            self.explosion_bound_right = bound_w - line_W
+            self.explosion_bound_top = self.bound_h
+            self.explosion_bound_bottom = self.bound_h * 2 - int(self.line_W * 1.5)
+            self.explosion_bound_left = self.line_W
+            self.explosion_bound_right = self.bound_w - self.line_W
 
-            self.position_y = bottom_y
+            self.position_y = self.bottom_y
             
         elif(num == 3):
             self.line_left_spot_x = self.line_left_spot_x + self.line_offset
@@ -163,30 +169,18 @@ class client:
             self.up_thickness = self.thin
             self.left_thickness = self.thin
 
-            self.explosion_bound_top = bound_h
-            self.explosion_bound_bottom = bound_h * 2 - int(line_W * 1.5)
-            self.explosion_bound_left = bound_w
-            self.explosion_bound_right = bound_w *2 - int(line_W * 1.5)
+            self.explosion_bound_top = self.bound_h
+            self.explosion_bound_bottom = self.bound_h * 2 - int(self.line_W * 1.5)
+            self.explosion_bound_left = self.bound_w
+            self.explosion_bound_right = self.bound_w *2 - int(self.line_W * 1.5)
 
-            self.position_x = right_x
-            self.position_y = bottom_y
+            self.position_x = self.right_x
+            self.position_y = self.bottom_y
             
         self.fireman_bound_top = self.line_up_spot_y 
         self.fireman_bound_bottom = self.line_down_spot_y - 25
         self.fireman_bound_left = self.line_left_spot_x
         self.fireman_bound_right = self.line_right_spot_x - 25
-
-    def write_file(self,length,data):
-        self.img_count += 1
-        if(self.img_count == 1000):
-            self.img_count = 0
-        if(self.record_flag):
-            self.file.write("img"+str(self.img_count)+":"+str(length))
-            self.file.write(str(data))
-            self.file.write("\n")
-            self.count_data += 1
-            if(self.count_data == 10):
-                self.file.close()
 
 
     def blink_for_line(self):
@@ -205,10 +199,12 @@ class client:
     def set_help2(self,flag):
         if(flag):
             if(self.first_recv_help2):
+                self.help2_flag = True
                 self.first_recv_help2 = False
                 self.blink_count = 0
                 self.draw_line_blinking_flag = True
         else:
+            self.help2_flag = False
             self.draw_line_blinking_flag = False
             self.first_recv_help2 = True
 
@@ -238,7 +234,13 @@ class client:
         self.namespace_img = my_namespace_img
 
     def set_sos_flag(self, flag):
-        self.sos_flag = flag
+        if(flag):
+            if(self.first_recv_sos):
+                self.sos_flag = True
+                self.first_recv_sos = False
+        else:
+            self.sos_flag = False
+            self.first_recv_sos = True
 
     def brush_namespace_background(self):
         if(self.sos_flag):
@@ -290,6 +292,7 @@ class client:
         flag = False
         self.img_binary += recv_str
         self.remain_package_size = self.img_package_size - len(self.img_binary)
+        ###### image recv complete ######
         if(self.remain_package_size <= 0):
             flag = self.decode_img(self.img_binary[0:self.img_package_size])
             if(len(self.img_binary) > self.img_package_size):
@@ -302,6 +305,7 @@ class client:
         msg = ""
         self.msg_binary += recv_str
         self.remain_msg_size -= len(recv_str)
+        ###### msg recv complete ######
         if(len(self.msg_binary) >= self.msg_size):
             try:
                 msg = self.msg_binary[0:self.msg_size].decode().strip()
@@ -334,8 +338,8 @@ class client:
             else:
                 return self.img_show
         else:
-            return img_white
-        return img_white
+            return self.img_white
+        return self.img_white
 
     def read_combine_img(self):
         return self.img_combine
@@ -347,7 +351,7 @@ class client:
                 self.recv_ir_flag = False
                 data = np.fromstring(binary, dtype = 'uint8')
                 data = cv2.imdecode(data, 1)
-                self.img_ir = np.reshape(data, (height, weight, 3))
+                self.img_ir = np.reshape(data, (self.height, self.weight, 3))
                 return False
             elif(self.recv_flir_flag):
                 ###### decode flir value ######
@@ -359,17 +363,17 @@ class client:
                     self.in_danger_flag = True
                 data = np.reshape(data, (60,80,1))
                 ###### if over threshold, replace part of ir image with red or green color ######
-                dst = cv2.resize(data, (weight,height), interpolation= cv2.INTER_CUBIC)
+                dst = cv2.resize(data, (self.weight,self.height), interpolation= cv2.INTER_CUBIC)
                 dst = np.dstack([dst]*3)
                 tmp = self.img_ir.copy()
-                dst = cv2.warpPerspective(dst,matrix, (weight,height))
+                dst = cv2.warpPerspective(dst,self.matrix, (self.weight,self.height))
 
                 np.place(tmp, (dst > self.th_100), (0,0,255))
                 np.place(tmp, ((dst > self.th_70)&(dst <= self.th_100)), (163,255,197))
                 before_rotate_img = cv2.addWeighted(self.img_ir, 0.5, tmp, 0.5, 0)
                 
                 ###### rotate image ######
-                rotate_img = cv2.warpAffine(before_rotate_img, M, (weight,height))
+                rotate_img = cv2.warpAffine(before_rotate_img, self.M, (self.weight,self.height))
                 self.img_combine = rotate_img
                 ###### put the warning message on pic ######
                 if(self.in_danger_flag | self.in_explosion_flag):
@@ -396,7 +400,7 @@ class client:
         except Exception as e:
             print("error in decode image",e.args)
             ###### if decode image fail, show the white image ######
-            self.img_show = img_white
+            self.img_show = self.img_white
             self.img_binary = b''
             return False
         return False
